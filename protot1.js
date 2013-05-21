f10ed6fdf1e734cf028af2b1e63b2ca4693900e1
Marcelo Rossi <marcerossi21@gmail.com>
2013-05-21 17:57:52 -0300

    Refactor on Folders per Topic

---
+++ protot1.js
@@ -0,0 +1,6 @@
+
+var mikhail = Object.create(null)
+Object.defineProperty(mikhail, 'name', { value:        'Mikhail'
+                                       , writable:     true
+                                       , configurable: true
+                                       , enumerable:   true })
