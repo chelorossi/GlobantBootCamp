dca5eeb0b15aaa3e6fb0c6176a23db44826d5a81
Marcelo Rossi <marcerossi21@gmail.com>
2013-05-28 13:58:09 -0300

    -Major: performance consideration. Put all your script tags at the end of the body, before its closing tag. Each time the browser interprets JavaScript code it must stop rendering the page, because there is only one thread for the UI actions. By putting all the script tags at the end of the page you get a smoother experience for the user as the rendering is not interrupted -- DONE

---
+++ Topic1/js/Events.js
@@ -0,0 +1,48 @@
+
+
+        $(document).ready(function() {
+            alert('Hello World!');
+            $('#alias').focus();
+            $('#boton1').click(function() {                          
+              $.ajax({
+                url: 'http://bootcamp.aws.af.cm/welcome/Marcelo',
+                success: function(response) {    
+                  var resHighlight =  response.response.replace("Marcelo", "<span style="+"'color:green'"+">"+"Marcelo"+"</span>");                                
+                  $('#res').append(resHighlight);                              
+                } ,
+                //falta manejar errores
+                error: function() {
+                  $('#res').css('background-color', 'red');
+                }
+              });           
+            });
+
+            $('#boton2').click(function() {
+              $.ajax({
+                url: 'http://search.twitter.com/search.json',
+                data: 'q=html5',
+                dataType: 'jsonp',
+                success: function(response) {                    
+                  //console.log(response);
+                  var aux_markup = '';
+                  for (var i = response.results.length - 1; i >= 0; i--) {   
+                      aux_markup +=  '<span class="tweets">‏‏&rlm;</span>';
+                      aux_markup += '<p>User: '+response.results[i].from_user+'</p>';                                      
+                      aux_markup += '<p>Text: '+response.results[i].text+'</p>';
+                      aux_markup += '<p>Created_At: '+response.results[i].created_at+'</p>';
+                      aux_markup += '<p>Profile_image_url: '+response.results[i].profile_image_url+'</p></div>';                                            
+                      
+                  }; 
+                  $('#res2').append(aux_markup);  
+                  $('#over_lay').css('height', $(document).height()).show();              
+                } ,
+                
+                error: function() {
+                  alert('An error was generated');
+                  $('#res2').css('background-color', 'red');
+                }
+              });           
+            });
+
+
+        });      
