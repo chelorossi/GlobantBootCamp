e71873887bd14d567e5e9cd5c7e76117f427d980
Marcelo Rossi <marcerossi21@gmail.com>
2014-02-12 13:16:22 -0200

    Hasta topic 6 done

---
+++ Topic6/topic6.js
@@ -0,0 +1,58 @@
+///// JS TOPIC 3 Functionality /////////////
+
+function Movie () {
+  this.attributes = {};
+}
+
+Movie.prototype.set = function(key,value) {
+  this.attributes[key] = value;
+}
+
+
+Movie.prototype.get = function(key) {
+  return this.attributes[key];
+}
+
+Movie.prototype.play = function() {
+  pubsub.publish( "playing", this);
+}
+
+
+Movie.prototype.stop = function() {
+  pubsub.publish( "stopped", this);
+}
+
+
+var messagePlay = function (topic, movie) {
+    console.log( 'Playing: ' + movie.get('title') + '...');
+};
+
+var messageStop = function (topic, movie) {
+    console.log( 'stopped');
+};
+
+
+
+
+var subscription = pubsub.subscribe( "playing", messagePlay );
+var subscription2 = pubsub.subscribe( "stopped", messageStop );
+
+
+
+
+
+
+var fastandfurious = new Movie();
+fastandfurious.set('title', 'Fast & Furious');  
+fastandfurious.play();
+fastandfurious.stop();
+
+var terminator = new Movie();
+terminator.set('title', 'Terminator');
+terminator.play();
+terminator.stop();
+
+var scarface = new Movie();
+scarface.set('title', 'ScarFace');
+scarface.play();
+scarface.stop();
